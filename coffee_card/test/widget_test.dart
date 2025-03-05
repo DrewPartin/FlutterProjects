eebd201b4f72794061734aa6438d80e99fc10a07
Drew Partin <drewpartin@gmail.com>
2025-03-05 14:02:25 -0600

    updated test file

--- coffee_card/test/widget_test.dart
+++ coffee_card/test/widget_test.dart
@@ -9,12 +9,10 @@ import 'package:coffee_card/coffee_prefs.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:coffee_card/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const CoffeePrefs());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
