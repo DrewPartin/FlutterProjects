3502c1426b20849d726593fc2c92b918187ab0c8
Drew Partin <drewpartin@gmail.com>
2025-03-06 19:08:04 -0600

    updated layout, UI, and theming

--- flutter_rpg/lib/screens/home/home.dart
+++ flutter_rpg/lib/screens/home/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rpg/screens/home/character_card.dart';
+import 'package:flutter_rpg/shared/styled_button.dart';
+import 'package:flutter_rpg/shared/styled_text.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,17 +11,36 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List characters = ['mario', 'luigi', 'peach', 'toad', 'bowser', 'koopa'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Your Characters'),
+        title: const StyledTitle('Your Characters'),
         centerTitle: true,
       ),
       body: Container(
         padding: const EdgeInsets.all(16),
-        child: const Text('Home'),
-      )
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: characters.length,
+                itemBuilder: (_, index) {
+                  return CharacterCard(characters[index]);
+                },
+              ),
+            ),
+
+            StyledButton(
+              onPressed: () {},
+              child: const StyledHeading('Create New'),
+            )
+          ]
+        ),
+      ),
     );
   }
 }
